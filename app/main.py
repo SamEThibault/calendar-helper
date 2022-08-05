3ba2c90458340a2f1f9ea9c4b6af94364c4c7df7
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-08-05 14:04:03 -0400

    BUGGY implemented date/time correction

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import getpass
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from datetime import datetime
 
 from calendarAPI import callAPI
 
@@ -81,15 +82,57 @@ time.sleep(3)
 class Event:
     def __init__(self, name, day, startEnd, times):
         self.name = name
-        self.day = day.split(" ")[1]
-        self.startEnd = startEnd.split(" - ")
-        self.startDate = self.startEnd[0].replace("/", "-")
-        self.endDate = self.startEnd[1].replace("/", "-")
-        self.startTime = times.split(" ")[1][:-2]
-        self.endTime = times.split(" ")[3][:-2]
+        day = day.split(" ")[1].lower()
+        startEnd = startEnd.split(" - ")
+        startDate = startEnd[0].replace("/", "-")
+        endDate = startEnd[1].replace("/", "-")
+        startTime = times.split(" ")[1][:-2]
+        endTime = times.split(" ")[3][:-2]
 
-        self.startDateTime = self.startDate + 'T' + self.startTime + ':00-04:00'
-        self.endDateTime = self.startDate + 'T' + self.endTime + ':00-04:00'
+        # get the start and end AM/PM specification to ensure correct ISO formatting 
+        startAmPm = times.split(" ")[1][len(startTime):]
+        endAmPm = times.split(" ")[3][len(endTime):]
+
+        adjustedStartTime = startTime
+        adjustedEndTime = endTime
+
+        if startAmPm == "PM": 
+            originalHour = startTime.split(":")[0]
+            newHour = str(int(originalHour) + 12)
+            adjustedStartTime = startTime.replace(originalHour, newHour)
+        if endAmPm == "PM":
+            originalHour = endTime.split(":")[0]
+            newHour = str(int(originalHour) + 12)
+            adjustedEndTime = startTime.replace(originalHour, newHour)
+
+        # need to adjust the startDate with the day of week for the course
+        startDay = startDate.split("-")[2]
+        if day == "monday":
+            dayOffset = 6
+        elif day == "tuesday":
+            dayOffset = 0
+        elif day == "wednesday":
+            dayOffset = 1
+        elif day == "thursday":
+            dayOffset = 2
+        elif day == "friday":
+            dayOffset = 3
+
+        startDay = str(int(startDay) + dayOffset)
+        adjustedStartDate = startDate.replace(startDate.split("-")[2], startDay)
+
+        # ISO Date Format needed for API request
+        self.startDateTime = adjustedStartDate + "T" + adjustedStartTime + ":00-04:00"
+        self.endDateTime = adjustedStartDate + "T" + adjustedEndTime + ":00-04:00"
+
+    def printCourse(self):
+        print("-------------- Course Information ------------------")
+        print("Name: " + self.name)
+        print("Start Day: " + day)
+        print("Start Date and Time: " + self.startDateTime)
+        print("End Data and Time: " + self.endDateTime)
+        print("-------------- END ------------------")
+
 
 # testing the first course and first lecture time
 name = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_SCRTAB_DTLS$0").text
@@ -97,6 +140,7 @@ day = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_DAYS1$0").text
 startEnd = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_ST_END_DT1$0").text
 times = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_DAYSTIMES1$0").text
 course = Event(name, day, startEnd, times)
+course.printCourse()
 callAPI(course)
 
 # second lecture time
@@ -104,6 +148,6 @@ day = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_DAYS2$0").text
 startEnd = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_ST_END_DT2$0").text
 times = driver.find_element(By.ID, "DERIVED_SSR_FL_SSR_DAYSTIMES2$0").text
 course = Event(name, day, startEnd, times)
+course.printCourse()
 callAPI(course)
-
 exit()
